d955af7f52496b6096710fe20a96883285045495
Ali Bouknana <abouknan@c4r10p2.1337.ma>
2026-08-08 06:02:05 +0100

    rak tma

---
+++ channel/Channel.hpp
@@ -0,0 +1,49 @@
+#ifndef CHANNEL_HPP
+# define CHANNEL_HPP
+
+#include <string>
+#include <set>
+#include <vector>
+
+// #include "Client.hpp"
+
+class Channel
+{
+    private:
+        std::string           _name;
+        std::string           _topic;
+        std::string           _key;
+        size_t                _limit;
+
+        bool                  _inviteOnly;
+        bool                  _topicLocked;
+
+        std::set<Client *>    _members;
+        std::set<Client *>    _operators;
+        std::set<std::string> _invited;
+
+        bool                  _hasKey;
+        bool                  _hasLimit;
+
+        Channel(const Channel &other);
+        Channel &operator=(const Channel &other);
+    
+    public:
+        Channel(const std::string &name);
+        ~Channel();
+    
+        int                     join(Client &c, const std::string &key);
+        void                    part(Client &c, const std::string &reason);
+        void                    kick(Client &op, Client &target, const std::string &reason);
+        void                    invite(Client &op, const std::string &nick);
+        void                    setTopic(Client &c, const std::string &topic);
+    
+        void                    applyModes(Client &c, const std::string &str, const std::vector<std::string> &args);
+    
+        void                    broadcast(const std::string &msg, Client *except);
+    
+        bool                    isOp(Client &c) const;
+        bool                    isEmpty() const;
+};
+
+#endif
